56869c36b626add672e90e7cde39c9075c10bd14
broke2rich <blakewarren98@gmail.com>
2025-04-27 13:45:26 -0400

    cors fix 4

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/scan', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
+
 
 
